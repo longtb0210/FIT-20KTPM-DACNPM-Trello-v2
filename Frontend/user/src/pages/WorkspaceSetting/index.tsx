50c9b402e1709c69d80e1d35b507f4df351e898d
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-04-12 18:44:24 +0700

    add sidebar workspace in setting workspace

--- Frontend/user/src/pages/WorkspaceSetting/index.tsx
+++ Frontend/user/src/pages/WorkspaceSetting/index.tsx
@@ -1,7 +1,43 @@
 // pages/account-management.tsx
 import React, { useEffect, useState } from 'react'
 import { Settings } from './components'
+import SidebarCateWorkSpace from '../CategoryWorkspace/component/SidebarCateWorkspace'
 import { useTheme } from '../../components/Theme/themeContext'
+import Box from '@mui/material/Box'
+import { styled } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import IconButton from '@mui/material/IconButton'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+
+const drawerWidth = 250
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  })
+}))
+
+const BackButton = styled(IconButton)({
+  position: 'fixed',
+  top: 10,
+  left: 10,
+  zIndex: 999
+})
+
 
 export const WorkspaceSetting: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState<string>('')
@@ -21,9 +57,34 @@ export const WorkspaceSetting: React.FC = () => {
     minHeight:  'calc(100vh - 51px)'
   }
 
+  const [open, setOpen] = React.useState(true)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
   return (
     <div style={darkLightMode} className={``}>
-      <Settings />
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        {open ? (
+          <BackButton onClick={handleDrawerClose} edge="start" color="inherit" aria-label="close">
+            <ChevronLeftIcon />
+          </BackButton>
+        ) : (
+          <IconButton onClick={handleDrawerOpen} edge="start" color="inherit" aria-label="open">
+            <ChevronRightIcon />
+          </IconButton>
+        )}
+        <SidebarCateWorkSpace open={open} handleDrawerClose={handleDrawerClose} />
+        <Main open={open} sx={{ padding: 0 }}>
+          <Settings />
+        </Main>
+      </Box>
     </div>
   )
 }
