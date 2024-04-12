333593a209ebae75b8121051d5f016506734c1f4
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-04-12 17:56:12 +0700

    fix button open navbar in board

--- Frontend/user/src/pages/CategoryWorkspace/index.tsx
+++ Frontend/user/src/pages/CategoryWorkspace/index.tsx
@@ -82,7 +82,7 @@ export function CategoryWorkspace() {
               sx={{ mr: 2, ...(open && { display: 'none' }) }}
             >
               <div className='relative'>
-                <div className='group rounded-full border border-gray-300'>
+                <div style={{borderRadius: '50%', border: '2px solid #888', width: '36px', height: '36px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                   <ChevronRightIcon className='h-6 w-6' />
                 </div>
               </div>
