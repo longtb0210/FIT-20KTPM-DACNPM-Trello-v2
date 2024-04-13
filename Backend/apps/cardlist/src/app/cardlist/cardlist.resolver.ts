770fa2e3a6e8d0515a1ec5c4731509608059d74c
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-04-13 20:05:23 +0700

    feat: update

--- Backend/apps/cardlist/src/app/cardlist/cardlist.resolver.ts
+++ Backend/apps/cardlist/src/app/cardlist/cardlist.resolver.ts
@@ -1,13 +1,13 @@
-import { Field, ObjectType, Query, Resolver } from '@nestjs/graphql'
+import { Query, Resolver } from '@nestjs/graphql'
 import { CardlistService } from './services/cardlist.service'
-import { DbSchemas } from '@trello-v2/shared'
-import { Cardlist } from './entity/cardlist.entity'
 
-@Resolver((of) => Cardlist)
+import { CardList } from '@trello-v2/shared/dist/src/schemas/CardList'
+
+@Resolver()
 export class CardlistResolver {
   constructor(private cardlistService: CardlistService) {}
 
-  @Query((returns) => [Cardlist])
+  @Query()
   cardlists(): Promise<CardList[]> {
     return this.cardlistService.getAllCardlistGraphQL()
   }
