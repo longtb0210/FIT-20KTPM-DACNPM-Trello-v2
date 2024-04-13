01e8ee2cd447676538fa397a6ea1e8fc9c66d2a8
Lamlance <lamhoangdien113@gmail.com>
2024-04-13 23:03:34 +0700

    update desc

--- Backend/apps/cardlist/src/app/card/services/card.service.ts
+++ Backend/apps/cardlist/src/app/card/services/card.service.ts
@@ -67,7 +67,7 @@ export class CardService {
         $set: {
           ...(data.name ? { 'cards.$.name': data.name } : {}),
           ...(data.cover ? { 'cards.$.cover': data.cover } : {}),
-          ...(data.description ? { 'cards.$.cover': data.description } : {}),
+          ...(data.description ? { 'cards.$.description': data.description } : {}),
         },
       },
       { new: true },
